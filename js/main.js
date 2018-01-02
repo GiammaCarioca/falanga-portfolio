4c782746063d519c6126a9e470734b7ab4ee2287
GiammaCarioca <giammattey@gmail.com>
2018-01-02 13:03:16 -0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("#pointillisme").click(function(){
+		$("span.soon1").addClass("show");
+	});	
+	$("#metas").click(function(){
+		$("span.soon2").addClass("show");
+	});	
+});
